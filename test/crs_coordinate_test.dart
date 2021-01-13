881af1831bb7d0b2817cde2d647781252c5f774f
Tomas Johansson <github-tomas@programmerare.com>
2021-01-13 21:32:48 +0100

    Indentation only.

--- test/crs_coordinate_test.dart
+++ test/crs_coordinate_test.dart
@@ -218,15 +218,15 @@ void main() {
   */
 }
 
-  void _AssertEqual(CrsCoordinate crsCoordinate_1, CrsCoordinate crsCoordinate_2)  {
-    Assert.AreEqual(crsCoordinate_1.crsProjection, crsCoordinate_2.crsProjection);
-    double maxDifference = crsCoordinate_1.crsProjection.isWgs84() ? 0.000007 : 0.5; // the other (i.e. non-WGS84) value is using meter as unit, so 0.5 is just five decimeters difference
-    double diffLongitude = ((crsCoordinate_1.xLongitude - crsCoordinate_2.xLongitude)).abs();
-    double diffLatitude = ((crsCoordinate_1.yLatitude - crsCoordinate_2.yLatitude)).abs();            
-    
-    Assert.IsTrue(diffLongitude < maxDifference);
-    Assert.IsTrue(diffLatitude < maxDifference);
-  }
+void _AssertEqual(CrsCoordinate crsCoordinate_1, CrsCoordinate crsCoordinate_2)  {
+  Assert.AreEqual(crsCoordinate_1.crsProjection, crsCoordinate_2.crsProjection);
+  double maxDifference = crsCoordinate_1.crsProjection.isWgs84() ? 0.000007 : 0.5; // the other (i.e. non-WGS84) value is using meter as unit, so 0.5 is just five decimeters difference
+  double diffLongitude = ((crsCoordinate_1.xLongitude - crsCoordinate_2.xLongitude)).abs();
+  double diffLatitude = ((crsCoordinate_1.yLatitude - crsCoordinate_2.yLatitude)).abs();            
+  
+  Assert.IsTrue(diffLongitude < maxDifference);
+  Assert.IsTrue(diffLatitude < maxDifference);
+}
 
 String _myCustomToStringFunction(CrsCoordinate coordinate) {
   return "${coordinate.yLatitude} , ${coordinate.xLongitude}";
