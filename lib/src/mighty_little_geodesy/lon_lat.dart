e1ba044ab1de5b892532087f34fe5e5ba7377013
Tomas Johansson <github-tomas@programmerare.com>
2021-01-13 11:10:24 +0100

    Implemented the class 'LonLat' as Dart code (in this library being ported from C#.NET)

--- lib/src/mighty_little_geodesy/lon_lat.dart
+++ lib/src/mighty_little_geodesy/lon_lat.dart
@@ -9,12 +9,9 @@
 
 // This class was not part of the original 'MightyLittleGeodesy'
 // but the class 'GaussKreuger' has later been changed to return this 'LonLat' instead of array 'double[]'
-internal class LonLat {
-  public double LongitudeX { get; private set; }
-  public double LatitudeY { get; private set; }
+class LonLat {
+  double LongitudeX;
+  double LatitudeY;
   
-  public LonLat(double xLongitude, double yLatitude) {
-    this.LongitudeX = xLongitude;
-    this.LatitudeY = yLatitude;
-  }
+  LonLat(this.LongitudeX, this.LatitudeY);
 }
