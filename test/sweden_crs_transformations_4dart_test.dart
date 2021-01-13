3ce4ec88835e4b5d200a20440deebb155abdbe5b
Tomas Johansson <github-tomas@programmerare.com>
2021-01-13 15:45:53 +0100

    Deleted a generated and unused file, and added some comments to a generated test file.

--- test/sweden_crs_transformations_4dart_test.dart
+++ test/sweden_crs_transformations_4dart_test.dart
@@ -1,16 +1,26 @@
 import 'package:sweden_crs_transformations_4dart/sweden_crs_transformations_4dart.dart';
 import 'package:test/test.dart';
 
+// an example test file (which can be copied and pasted when creating a new test file to quickly the main structure of a test file)
+
+// this test file and the tested Awesome class (lib\src\sweden_crs_transformations_4dart_base.dart)
+// used below was generated as example code when the 
+// project was generated with the following dart command:
+// dart create -t package-simple sweden_crs_transformations_4dart
+// but since then the file is no longer exported i.e. the following line has been removed from sweden_crs_transformations_4dart.dart:
+//  export 'src/sweden_crs_transformations_4dart_base.dart';
+// so therefore the code below is disabled with
+
 void main() {
   group('A group of tests', () {
-    Awesome awesome;
+    // Awesome awesome; // used to exist in the generated file 'lib\src\sweden_crs_transformations_4dart_base.dart'
 
     setUp(() {
-      awesome = Awesome();
+      // awesome = Awesome(); 
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      // expect(awesome.isAwesome, isTrue);
     });
   });
 }
